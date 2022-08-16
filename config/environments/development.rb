a6a9a93d542c48f62d492999213cb1bae5fd207d
EC2 Default User <ec2-user@ip-172-31-18-190.ap-northeast-1.compute.internal>
2022-08-16 12:09:49 +0000

    ([Edit] routesの編集 home#topへの自動遷移実装 )

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
+  config.hosts << "8c867b29df8a40128e5a812f84a9cac4.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'bookers/new'
-  get 'bookers/index'
-  get 'bookers/show'
-  get 'bookers/edit'
-  get 'homes/top'
-  get 'homes/about'
+
   devise_for :users
+  root to: 'homes#top'
+
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :homes, only: [:top, :about]
+  resources :bookers, only: [:new, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
