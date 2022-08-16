207ce0911e00c49486fa1b0ff78fbef37ea1c651
EC2 Default User <ec2-user@ip-172-31-18-190.ap-northeast-1.compute.internal>
2022-08-16 11:32:25 +0000

    ([Finish] ５～１５完了)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'bookers/new'
+  get 'bookers/index'
+  get 'bookers/show'
+  get 'bookers/edit'
+  get 'homes/top'
+  get 'homes/about'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
